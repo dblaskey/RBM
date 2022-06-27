0263676ffc8e879f32a458cc842f7d7bc53f23f8
Dylan Blaskey <67331267+dblaskey@users.noreply.github.com>
2022-06-27 11:18:09 -0600

    Update Begin.f90

--- src/Begin.f90
+++ src/Begin.f90
@@ -262,9 +262,9 @@ if(ns_max_test.gt.ns_max) then
     stop
 end if
 !
-nwpd=1
+nwpd=1 ! comment this out an have number of timesteps per day read from the file otherwise I can just hardwire 8
 xwpd=nwpd
-dt_comp=86400./xwpd
+dt_comp=86400./xwpd 
 !
 !     ******************************************************
 !                         Return to RMAIN
