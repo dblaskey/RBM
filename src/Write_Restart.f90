6ef08dd25a8bbf7e304615b791bd7ecde8f094ba
Dylan Blaskey <67331267+dblaskey@users.noreply.github.com>
2022-07-26 13:46:47 -0600

    Update Write_Restart.f90

--- src/Write_Restart.f90
+++ src/Write_Restart.f90
@@ -1,13 +1,13 @@
-SUBROUTINE WRITE_RESTART(nr,ncell,ns,T_0,T_head)
+SUBROUTINE WRITE_RESTART(nr,ncell,nseg_temp,T_0,T_head)
 !
 Implicit NONE
 !
-integer :: nr,ncell,ns 
+integer :: nr,ncell,nseg_temp 
 real    :: T_0
 real(8) :: time
 real    :: T_head
 !
 write (19,'(3i6,2f8.2)')           &
-            nr,ncell,ns,T_0,T_head
+            nr,ncell,nseg_temp,T_0,T_head
 close(19)
 end SUBROUTINE WRITE_RESTART
