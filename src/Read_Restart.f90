a31a58371c6806670d26b920f5e2509dc3995656
Dylan Blaskey <67331267+dblaskey@users.noreply.github.com>
2022-07-26 15:56:48 -0600

    Update Read_Restart.f90

--- src/Read_Restart.f90
+++ src/Read_Restart.f90
@@ -1,1 +1,17 @@
-! Here it is
+SUBROUTINE Read_Restart(nyear,nr) ! NOT CURRENTLY USED
+!
+IMPLICIT NONE
+!
+integer :: nr,ncell,nseg_temp 
+real    :: T_0
+real(8) :: time
+real    :: T_head
+
+allocate (temp(nreach,0:ns_max,2))
+allocate (T_head(nreach))
+
+!
+open (19, file = 'data1.dat', status = 'old')
+read(19,*) nr,ncell,nseg_temp,T_0,T_head
+!
+END SUBROUTINE Read_Restart
