f21e8fc4b2bd84e8364ec3f7da9416df3fe19520
Dylan Blaskey <67331267+dblaskey@users.noreply.github.com>
2022-07-26 13:22:55 -0600

    Update Write_Restart.f90

--- src/Write_Restart.f90
+++ src/Write_Restart.f90
@@ -1,20 +1,12 @@
-SUBROUTINE WRITE_RESTART(outPrefix,nyear,nr,ncell,ns,T_0,T_head)
+SUBROUTINE WRITE_RESTART(nr,ncell,ns,T_0,T_head)
 !
 Implicit NONE
 !
-integer :: nyear,nr,ncell,ns 
+integer :: nr,ncell,ns 
 real    :: T_0
 real(8) :: time
 real    :: T_head
-character (len=200 ):: restart_file
-character(len=10) :: file_id
-character (len=200 ):: outPrefix
 !
-! Write the integer into a string:
-write(file_id, '(i0)') nyear
-!
-restart_file=TRIM(outPrefix)//'_'//TRIM(ADJUSTL(file_id))//'.r'
-open(19,file=TRIM(restart_file),status='unknown')
 write (19,'(3i6,2f8.2)')           &
             nr,ncell,ns,T_0,T_head
 close(19)
